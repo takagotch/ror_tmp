377ddac44728cf95884882f0670338dfd3fff4a6
takagotch <dyaccb@gmail.com>
2018-01-26 05:35:05 +0900

    config.omniauth_path_prefix = '/my_engine/users/auth'

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :app, "admin@#{fetch(:app_eip)}"
+set :web, "admin@#{fetch(:app_eip)}"
+set :db, "admin@#{fetch(:app_eip)}"
+
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,15 @@
-#config.authentication_keys = [:email]
-config.authentication_keys = [:login]
+Devise.setup do |config|
+  config.secret_key = Rails.application.secret.SECRET_TOKEN
+  config.mailer_sender = 'please-change-me-at-config-initializers-decise@ex,com'
+  #config.mailer_sender = 'Devise::Mailer'
+  #config.parent_mailer = 'ActionMailer::Base'
+  require 'devise/orm/active_record'
+  #config.authentication_keys = [:email]
+  config.authentication_keys = [:login]
+  #config.request_keys = []
+  #
+  #
+  #
+  #config.omniauth_path_prefix = '/myengine/users/auth'
+end
 
