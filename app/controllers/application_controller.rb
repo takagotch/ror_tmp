ca7deeeab7a56c5f38bc76de1fc3ead370c2aa99
takagotch <dyaccb@gmail.com>
2018-01-25 12:03:07 +0900

    configure_permitted_parameters,

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActiveController::Base
   protect_form_forgery with: :exception, prepend: true
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[name password password_confirmation remember_me]
+    device_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    device_parameter_sanitizer.permit :account_update, keys: addded_attrs
+  end
 end
 
